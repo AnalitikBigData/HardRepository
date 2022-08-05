c9cdc9ced2a72b9145d01bf4b4f33c97a3bb9293
Veronika <morozznika@gmail.com>
2022-08-05 22:27:26 +0300

    изменение функции проверки на число

--- scriptHard.js
+++ scriptHard.js
@@ -1,53 +1,8 @@
-let str = ' dkdl opqqq qqqq q xz @Mial asa ';
+let variable = '  123  ';
 
-
-const workWithStr = function(strNew){
-    if(typeof(strNew)!== 'string'){
-        return 'не строка';
-    }
-    else{
-        let strTemp1 = '-', strTemp2='-', result;
-        if(strNew[0] === ' '){
-            strTemp1 = strNew[0].replace(' ', '');
-        }
-        if(strNew[strNew.length-1] === ' '){
-            strTemp2 = strNew[strNew.length-1].replace(' ', '');
-        }
-        if(strTemp1 === '' && strTemp2 === ''){
-            result = strTemp1 + strNew.slice(1, strNew.length - 1) + strTemp2;
-            if (result.length > 30 ){
-                return result.slice(0, 30) + '...';
-            }
-            else{
-                return result;
-            }
-        }
-        else if(strTemp1 !== '' && strTemp2 === ''){
-            result = strNew.slice(0, strNew.length - 1) + strTemp2;
-            if (result.length > 30 ){
-                return result.slice(0, 30) + '...';
-            }
-            else{
-                return result;
-            }
-        }
-        else if(strTemp1 === '' && strTemp2 !== ''){
-            result = strTemp1 + strNew.slice(1);
-            if (result.length > 30 ){
-                return result.slice(0, 30) + '...';
-            }
-            else{
-                return result;
-            }
-        }
-        else{
-            if (strNew.length > 30 ){
-                return strNew.slice(0, 30) + '...';
-            }
-            else{
-                return strNew;
-            }
-        }
-    }
+const Number = function(num){
+    return !isNaN(num) && isFinite(num);
 }
-console.log(workWithStr(str));
+
+
+console.log("== " + Number(String(variable).trim()));
