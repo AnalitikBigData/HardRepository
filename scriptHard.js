2ffd08da8a9350de03fa438b3602fe5db3b23b69
Veronika <morozznika@gmail.com>
2022-08-06 18:01:50 +0300

    перевод строки в число

--- scriptHard.js
+++ scriptHard.js
@@ -1,8 +1,23 @@
-let variable = '  123  ';
+let variable = +prompt();
+console.log(variable);
+let str = String(variable);
 
 const Number = function(num){
     return !isNaN(num) && isFinite(num);
 }
 
+if (str.trim() === ''){
+    console.log('не число' + str);
+}
+else if(str === null){
+    console.log('не число ' + str);
+}
+else if (!Number(String(variable).trim())){
+    console.log('не число ' + str);
+}
+else if (Number(String(variable).trim())){
+    console.log('число ' + str.trim());
+    variable = +str.trim();
+}
 
-console.log("== " + Number(String(variable).trim()));
+console.log('variable = ' + variable + ' type - '+ typeof(variable) );
