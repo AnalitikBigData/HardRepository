61c5715ba72899649ab85c1f5785ee934d098c02
Veronika <morozznika@gmail.com>
2022-08-07 19:06:40 +0300

    расширен функционал бота- количество попыток максимально 10

--- scriptHard.js
+++ scriptHard.js
@@ -1,23 +1,57 @@
-let variable = +prompt();
-console.log(variable);
-let str = String(variable);
+'use strict';
+alert('Загадывание случайного числа от 1 до 100');
 
 const Number = function(num){
-    return !isNaN(num) && isFinite(num);
+    return !isNaN(parseFloat(num)) && isFinite(num);
 }
 
-if (str.trim() === ''){
-    console.log('не число' + str);
+const getMessage = function(x){
+    let y = 21;
+    let numberOfAttempts = 10; // всего попыток
+    let numberOfUserAttempts = 0; // пользователь ввёл 
+    const func = function(){
+        if(numberOfUserAttempts < 10){
+            numberOfUserAttempts++;
+            numberOfAttempts--;
+            if(confirm('Играем')===true){
+                if(x > 100){
+                    //x = +prompt('Введите число ещё раз');
+                    console.log('Загаданное число меньше, осталось попыток ' + numberOfAttempts);
+                    x = +prompt('Введите число ещё раз');
+                    func();
+                }
+                else if(x < 1){
+                    console.log('Загаданное число больше, осталось попыток ' + numberOfAttempts);
+                    x = +prompt('Введите число ещё раз');
+                    func();
+                } else if(!Number(x)){
+                    console.log('Введи число!, осталось попыток ' + numberOfAttempts +  ' x = ' + x + ' '+ typeof(x));
+                    x = +prompt('Введите число ещё раз');
+                    func();
+                } 
+                else if(x !== y ){
+                    console.log('Неверно , осталось попыток ' + numberOfAttempts +  ' x != ' + x + typeof(x));
+                    x = +prompt('Введите число ещё раз');
+                    func();
+                }
+                else if(x === y){
+                    console.log('Поздравляю, Вы угадали!!! ' + x);
+                }
+            }
+            else {
+                console.log('Игра окончена');
+            }
+        } else {
+            let str = confirm('Попытки закончились, хотите сыграть еще?');
+            if (str === true){
+                func();
+            }
+            else{
+                console.log('Игра окончена');
+            }
+            //console.log('Попытки закончились, хотите сыграть еще?');
+        }
+    }
+    func();
 }
-else if(str === null){
-    console.log('не число ' + str);
-}
-else if (!Number(String(variable).trim())){
-    console.log('не число ' + str);
-}
-else if (Number(String(variable).trim())){
-    console.log('число ' + str.trim());
-    variable = +str.trim();
-}
-
-console.log('variable = ' + variable + ' type - '+ typeof(variable) );
+console.log(getMessage(+prompt('Введите число')));
