3533b9ce963d1eeaf6a7d2115d03e4db45a33c18
Veronika <morozznika@gmail.com>
2022-08-10 17:10:19 +0300

    дни недели - вывод

--- scriptjs.js
+++ scriptjs.js
@@ -1,37 +1,19 @@
-const array = ['1353389393', '3788493922', '2433567855', '478322222', '121212000', '2355611', '4822226620'];
-for(let i = 0; i < 7; i++){
-    if(array[i][0] === '2' || array[i][0] === '4'){
-        console.log(array[i]);
-    }
-}
-
-// простые числа от 1 до 100
-const isPrime = function(num){
-    count = 0;
-    for(let i = 2; i < num; i++){
-        if(num % i === 0){
-            count += 1;
-        }
-    }
-    if(num === 1){
-        return false;
-    }
-    else if (num === 2){
-        return true;
-    }
+'use strict';
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+let now = new Date();
+alert (now);
+alert(now.getDay());
+for (let i = 0; i < week.length; i++){
+    let day = week[i];
+    if (i === 5 || i === 6){
+        day = week[i].italics();
+        console.log(day);
+    } 
+    else if (now.getDay() === i + 1){
+        day = week[i].bold();
+        console.log(day);
+    } 
     else{
-        if(count != 0){
-            return false;
-        }
-        else{
-            return true;
-        }
-    }
-    
-}
-
-for(let i = 1; i <= 100; i++){
-    if(isPrime(i) === true){
-        console.log('Делители этого числа: 1 и ' + i);
+        console.log(day);
     }
 }
