6e850c23936426341d45e56c94920b516b469b45
Veronika <morozznika@gmail.com>
2022-08-15 16:55:11 +0300

    если текущий день выходной

--- scriptjs.js
+++ scriptjs.js
@@ -8,7 +8,12 @@ for (let i = 0; i < week.length; i++){
      
     if (now.getDay() === i + 1){
         day = week[i].bold();
-        console.log(day);
+        if(now.getDay() === 5 || now.getDate() === 6) {
+            day = week[i].italics();
+            console.log(day);
+        }
+        else console.log(day);
+         
     } 
     else if (i === 5 || i === 6){
         day = week[i].italics();
